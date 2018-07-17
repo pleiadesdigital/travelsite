060ea0525b7e3c516b3d6c49e9e413511e764136
Ronaldo Ortiz <ronyortizop@yahoo.com>
2018-07-17 13:56:17 -0400

    vid 012 done

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default', function(){
+  console.log('Hooray, you created a simple task!');
+});
+
+gulp.task('html', function(){
+  console.log('Imagine something useful being done to your html!');
+});
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'))
+});
+
+gulp.task('watch', function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html'); });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  });
+
+});
