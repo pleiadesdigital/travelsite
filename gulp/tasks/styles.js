85f2897dd27d1e3da4bdb2438f81962db954e263
Ronaldo Ortiz <ronyortizop@yahoo.com>
2018-08-25 20:18:09 -0400

    Testimonial Section done

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var maps = require('gulp-sourcemaps');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
-var mixins = require('postcss-mixins')  ;
+var mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
+    .pipe(maps.init())
     .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(maps.write('.'))
     .on('error', function(errorInfo){
       console.log(errorInfo.toString());
       this.emit('end');
