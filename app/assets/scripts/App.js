c72d555680b56725741a6d13bd52e7dbe57a1c51
Ronaldo Ortiz <ronyortizop@yahoo.com>
2018-09-02 13:05:15 -0400

    VID 41 DONE AND WORKING

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,12 @@
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function() {
-    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.color + ".");
-  }
-}
+var $ = require('jquery');
+var Person = require('./modules/Person');
+
+alert("ABC 321");
 
 var john = new Person("John Doe", "color");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
