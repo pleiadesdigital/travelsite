365e08b71c66db1c5568671bd63ec6e5a7807d5f
Ronaldo Ortiz <ronyortizop@yahoo.com>
2018-09-03 20:58:38 -0400

    Mobile menu completed - VID 45 done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,4 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person'); node.js way
-import Person from './modules/Person'; // native JS way
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes!");
-  }
-}
+var mobileMenu = new MobileMenu();
 
-var john = new Person("John Doe", "color");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
